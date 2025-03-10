3ba0d0cf2893a96aaae218b4139a44b2bdd2c114
xijiu9 <328659100@qq.com>
2025-03-11 07:03:42 +0800

    [minor] Remove hard-coded attention mask

--- svg/models/cog/utils.py
+++ svg/models/cog/utils.py
@@ -55,8 +55,30 @@ def sparsity_to_width(sparsity, context_length, num_frame, frame_size):
 
 def get_attention_mask(mask_name, context_length, num_frame, frame_size):
     # TODO: Replace with real implementation
+
+    attention_mask = torch.zeros((context_length + num_frame * frame_size, context_length + num_frame * frame_size)).cuda()
     if mask_name == "spatial":
-        attention_mask = torch.load("/data/home/xihaocheng/andy_develop/I2VSparse/sparseattn/v1.5/mask_tensor/mask_spatial.pt", map_location="cuda")
+        attention_mask[:context_length, :] = 1
+        attention_mask[:, :context_length] = 1
+        block_size, block_thres = 128, frame_size * 1.5
+        num_block = math.ceil(num_frame * frame_size / block_size)
+        for i in range(num_block):
+            for j in range(num_block):
+                if abs(i - j) < block_thres // block_size:
+                    attention_mask[i * block_size : (i + 1) * block_size, j * block_size : (j + 1) * block_size] = 1
+        # attention_mask = torch.load("/data/home/xihaocheng/andy_develop/I2VSparse/sparseattn/v1.5/mask_tensor/mask_spatial.pt", map_location="cuda")
     elif mask_name == "temporal":
-        attention_mask = torch.load("/data/home/xihaocheng/andy_develop/I2VSparse/sparseattn/v1.5/mask_tensor/mask_temporal.pt", map_location="cuda")
+        pixel_attn_mask = torch.zeros_like(attention_mask[context_length:, context_length:])
+
+        block_size, block_thres = 128, frame_size * 1.5
+        num_block = math.ceil(num_frame * frame_size / block_size)
+        for i in range(num_block):
+            for j in range(num_block):
+                if abs(i - j) < block_thres // block_size:
+                    pixel_attn_mask[i * block_size : (i + 1) * block_size, j * block_size : (j + 1) * block_size] = 1
+
+        pixel_attn_mask = pixel_attn_mask.reshape(frame_size, num_frame, frame_size, num_frame)\
+            .permute(1, 0, 3, 2).reshape(frame_size * num_frame, frame_size * num_frame)
+        attention_mask[context_length:, context_length:] = pixel_attn_mask
+        # attention_mask = torch.load("/data/home/xihaocheng/andy_develop/I2VSparse/sparseattn/v1.5/mask_tensor/mask_temporal.pt", map_location="cuda")
     return attention_mask
